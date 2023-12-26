ac440ecd3769ede543bf8190e5eb4cb523f1710c
RemyDegenne <Remydegenne@gmail.com>
2023-12-26 13:49:04 +0100

    add doc-gen to lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -21,10 +21,17 @@ def weakLeanArgs : Array String :=
 
 package «clt» where
   moreServerArgs := moreServerArgs
+  leanOptions := #[
+    ⟨`relaxedAutoImplicit, true⟩, -- prevents typos to be interpreted as new free variables
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩]
   -- add any package configuration options here
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 
 @[default_target]
 lean_lib «Clt» where

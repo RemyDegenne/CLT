9383c6f4f186f7a0df1ae86fbc3a261d45429805
Enrico Zandomeni Borba <enricozb@gmail.com>
2024-04-27 07:58:33 +0200

    clean lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,40 +1,10 @@
 import Lake
 open Lake DSL
 
-def moreServerArgs := #[
-  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
-  "-Dpp.proofs.withType=false"
-]
+package «clt»
 
--- These settings only apply during `lake build`, but not in VSCode editor.
-def moreLeanArgs := moreServerArgs
-
--- These are additional settings which do not affect the lake hash,
--- so they can be enabled in CI and disabled locally or vice versa.
--- Warning: Do not put any options here that actually change the olean files,
--- or inconsistent behavior may result
-def weakLeanArgs : Array String :=
-  if get_config? CI |>.isSome then
-    #["-DwarningAsError=true"]
-  else
-    #[]
-
-package «clt» where
-  moreServerArgs := moreServerArgs
-  leanOptions := #[
-    ⟨`relaxedAutoImplicit, true⟩, -- prevents typos to be interpreted as new free variables
-    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
-    ⟨`pp.proofs.withType, false⟩]
-  -- add any package configuration options here
+lean_lib «CLT»
+lean_lib «MathlibExt»
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
-
-meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
-
-@[default_target]
-lean_lib «Clt» where
-  moreLeanArgs := moreLeanArgs
-  weakLeanArgs := weakLeanArgs
-  -- add any library configuration options here

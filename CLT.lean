7cdd045fed4a19cfb4475ca9968e020894939d95
Enrico Zandomeni Borba <enricozb@gmail.com>
2024-04-27 08:26:40 +0200

    Clt.lean -> CLT.lean

---
+++ CLT.lean
@@ -0,0 +1,9 @@
+-- This module serves as the root of the `CLT` library.
+-- Import modules here that should be built as part of the library.
+
+-- import CLT.Tight
+-- import CLT.Separating
+-- import CLT.CharFun
+-- import CLT.ExpPoly
+
+import Mathlib
--- Clt.lean
+++
@@ -1,6 +0,0 @@
--- This module serves as the root of the `Clt` library.
--- Import modules here that should be built as part of the library.
-import Clt.Tight
-import Clt.Separating
-import Clt.CharFun
-import Clt.ExpPoly

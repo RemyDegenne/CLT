f5a1324d780908fd8dd0897d00991907e6a174c3
Remy Degenne <remydegenne@gmail.com>
2025-02-17 09:17:26 +0100

    require checkdecls

--- lakefile.lean
+++ lakefile.lean
@@ -30,6 +30,8 @@ package «clt» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
 

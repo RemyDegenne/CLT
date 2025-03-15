4b0b53bd09c34d057d55d1a96ef06dc855f3f3a1
Remy Degenne <remydegenne@gmail.com>
2025-03-15 09:44:03 +0100

    add import for blueprint purposes

--- Clt/CLT.lean
+++ Clt/CLT.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Thomas Zhu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Thomas Zhu, Rémy Degenne
 -/
+import Mathlib.MeasureTheory.Measure.Tight
 import Mathlib.Probability.IdentDistrib
 import Clt.Gaussian
 import Clt.Inversion

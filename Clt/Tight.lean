5405cf95d8f08c1ab3464d1f7deae7f5eb26cbc3
Remy Degenne <remydegenne@gmail.com>
2025-03-16 20:51:36 +0100

    fix name capitalization

--- Clt/Tight.lean
+++ Clt/Tight.lean
@@ -17,19 +17,19 @@ open scoped ENNReal Topology
 variable {E ι : Type*} {mE : MeasurableSpace E} [NormedAddCommGroup E] [InnerProductSpace ℝ E]
   {μ : ι → Measure E} [∀ i, IsProbabilityMeasure (μ i)]
 
-lemma equiContinuousAt_charFun_zero_of_isTightMeasureSet (hμ : IsTightMeasureSet {μ i | i}) :
+lemma equicontinuousAt_charFun_zero_of_isTightMeasureSet (hμ : IsTightMeasureSet {μ i | i}) :
     EquicontinuousAt (fun i ↦ charFun (μ i)) 0 := by
   sorry
 
-lemma isTightMeasureSet_of_equiContinuousAt_charFun
+lemma isTightMeasureSet_of_equicontinuousAt_charFun
     (hμ : EquicontinuousAt (fun i ↦ charFun (μ i)) 0) :
     IsTightMeasureSet {μ i | i} := by
   sorry
 
-lemma isTightMeasureSet_iff_equiContinuousAt_charFun :
+lemma isTightMeasureSet_iff_equicontinuousAt_charFun :
     IsTightMeasureSet {μ i | i} ↔ EquicontinuousAt (fun i ↦ charFun (μ i)) 0 :=
-  ⟨equiContinuousAt_charFun_zero_of_isTightMeasureSet,
-    isTightMeasureSet_of_equiContinuousAt_charFun⟩
+  ⟨equicontinuousAt_charFun_zero_of_isTightMeasureSet,
+    isTightMeasureSet_of_equicontinuousAt_charFun⟩
 
 /-- Let $(\mu_n)_{n \in \mathbb{N}}$ be measures on $\mathbb{R}^d$ with characteristic functions
 $(\hat{\mu}_n)$. If $\hat{\mu}_n$ converges pointwise to a function $f$ which is continuous at 0,

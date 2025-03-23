ef2e7874d01a84c4badbc399829fb0545cc00567
Remy Degenne <remydegenne@gmail.com>
2025-03-23 11:20:19 +0100

    add aux lemma

--- Clt/Inversion.lean
+++ Clt/Inversion.lean
@@ -32,6 +32,47 @@ theorem MeasureTheory.ProbabilityMeasure.ext_of_charFun_eq (μ ν : ProbabilityM
 
 end FromMathlibPR19761
 
+theorem ext_of_forall_mem_subalgebra_integral_eq_of_pseudoEMetric_complete_countable'
+    {E 𝕜 : Type*} [RCLike 𝕜] [MeasurableSpace E]
+    [PseudoEMetricSpace E] [BorelSpace E] [CompleteSpace E] [SecondCountableTopology E]
+    {P P' : Measure E} [IsFiniteMeasure P] [IsFiniteMeasure P']
+    {A : StarSubalgebra 𝕜 (E →ᵇ 𝕜)} (hA : (A.map (toContinuousMapStarₐ 𝕜)).SeparatesPoints)
+    (heq : ∀ (g : E →ᵇ ℝ),
+      (ofRealAm (K := 𝕜)).compLeftContinuousBounded ℝ lipschitzWith_ofReal g ∈ A →
+        ∫ x, g x ∂P = ∫ x, g x ∂P') :
+    P = P' := by
+  --consider the real subalgebra of the purely real-valued elements of A
+  let A_toReal := (A.restrictScalars ℝ).comap
+    (ofRealAm.compLeftContinuousBounded ℝ lipschitzWith_ofReal)
+  --the real subalgebra separates points
+  have hA_toReal : (A_toReal.map (toContinuousMapₐ ℝ)).SeparatesPoints := by
+    rw [RCLike.restrict_toContinuousMap_eq_toContinuousMapStar_restrict]
+    exact Subalgebra.SeparatesPoints.rclike_to_real hA
+  --integrals of elements of the real subalgebra wrt P, P', respectively, coincide
+  have heq' : ∀ g ∈ A_toReal, ∫ x, g x ∂P = ∫ x, g x ∂P' := by
+    intro g hgA_toReal
+    rw [← @ofReal_inj 𝕜, ← integral_ofReal, ← integral_ofReal]
+    have hg_eq := heq g hgA_toReal
+    norm_cast
+  apply ext_of_forall_integral_eq_of_IsFiniteMeasure
+  intro f
+  have h0 : Tendsto (fun ε : ℝ => 6 * sqrt ε) (𝓝[>] 0) (𝓝 0) := by
+    nth_rewrite 3 [← mul_zero 6]
+    apply tendsto_nhdsWithin_of_tendsto_nhds (Tendsto.const_mul 6 _)
+    nth_rewrite 2 [← sqrt_zero]
+    exact Continuous.tendsto continuous_sqrt 0
+  have lim1 : Tendsto (fun ε => |∫ x, mulExpNegMulSq ε (f x) ∂P - ∫ x, mulExpNegMulSq ε (f x) ∂P'|)
+      (𝓝[>] 0) (𝓝 0) := by
+    apply squeeze_zero' (eventually_nhdsWithin_of_forall (fun x _ => abs_nonneg _))
+      (eventually_nhdsWithin_of_forall _) h0
+    exact fun ε hε => dist_integral_mulExpNegMulSq_comp_le f hA_toReal heq' hε
+  have lim2 : Tendsto (fun ε => |∫ x, mulExpNegMulSq ε (f x) ∂P
+      - ∫ x, mulExpNegMulSq ε (f x) ∂P'|) (𝓝[>] 0)
+      (𝓝 |∫ x, f x ∂↑P - ∫ x, f x ∂↑P'|) :=
+    Tendsto.abs (Tendsto.sub (tendsto_integral_mulExpNegMulSq_comp f)
+      (tendsto_integral_mulExpNegMulSq_comp f))
+  exact eq_of_abs_sub_eq_zero (tendsto_nhds_unique lim2 lim1)
+
 lemma MeasureTheory.ProbabilityMeasure.tendsto_of_tight_of_separatesPoints
     {E 𝕜 : Type*} [RCLike 𝕜] [MeasurableSpace E]
     [MetricSpace E] [CompleteSpace E] [SecondCountableTopology E] [BorelSpace E]

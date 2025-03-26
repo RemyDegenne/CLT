aaaf28a39821072c4de7392a8e3d6f8d7af9d507
Remy Degenne <remydegenne@gmail.com>
2025-03-26 18:32:14 +0100

    import reduction

--- Clt/Tight.lean
+++ Clt/Tight.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import Clt.CharFun
-import Clt.Prokhorov
 
 /-!
 # Tightness and characteristic functions

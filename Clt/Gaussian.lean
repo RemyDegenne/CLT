f110459f0382d847e3801004376b6d1f2d8d1171
Remy Degenne <remydegenne@gmail.com>
2025-04-24 15:30:06 +0200

    refactor: remove meanMap and varMap

--- Clt/Gaussian.lean
+++ Clt/Gaussian.lean
@@ -42,6 +42,26 @@ lemma pow_toReal_eLpNorm {E F : Type*} {mE : MeasurableSpace E} {μ : Measure E}
   have h := rpow_toReal_eLpNorm h_Lp (by positivity)
   simpa using h
 
+@[simp]
+lemma variance_id_dirac {E : Type*} {mE : MeasurableSpace E} [MeasurableSingletonClass E]
+    (X : E → ℝ) (x : E) :
+    Var[X ; Measure.dirac x] = 0 := by
+  rw [variance_eq_integral]
+  · simp
+  · exact aemeasurable_dirac
+
+lemma variance_id_map {E : Type*} {mE : MeasurableSpace E} {μ : Measure E}
+    {f : E → ℝ} (hf : AEMeasurable f μ) :
+    Var[id ; μ.map f] = Var[f ; μ] := by
+  rw [variance_eq_integral measurable_id.aemeasurable, integral_map hf]
+  swap; · exact Measurable.aestronglyMeasurable <| by fun_prop
+  simp only [id_eq]
+  rw [variance_eq_integral hf]
+  congr with x
+  congr
+  rw [integral_map hf]
+  exact Measurable.aestronglyMeasurable <| by fun_prop
+
 end Aux
 
 namespace ProbabilityTheory
@@ -70,79 +90,17 @@ section Def
 variable {E : Type*} [TopologicalSpace E] [AddCommMonoid E] [Module ℝ E] {mE : MeasurableSpace E}
 
 class IsGaussian (μ : Measure E) : Prop where
-  map_eq_gaussianReal : ∀ L : E →L[ℝ] ℝ, ∃ m v, μ.map L = gaussianReal m v
-
-def _root_.MeasureTheory.Measure.meanMap (μ : Measure E) [IsGaussian μ] (L : E →L[ℝ] ℝ) : ℝ :=
-  (IsGaussian.map_eq_gaussianReal (μ := μ) L).choose
-
-def _root_.MeasureTheory.Measure.varMap (μ : Measure E) [IsGaussian μ] (L : E →L[ℝ] ℝ) :
-    ℝ≥0 :=
-  (IsGaussian.map_eq_gaussianReal (μ := μ) L).choose_spec.choose
-
-lemma _root_.MeasureTheory.Measure.map_eq_gaussianReal (μ : Measure E) [IsGaussian μ]
-    (L : E →L[ℝ] ℝ) :
-    μ.map L = gaussianReal (μ.meanMap L) (μ.varMap L) :=
-  (IsGaussian.map_eq_gaussianReal L).choose_spec.choose_spec
+  map_eq_gaussianReal : ∀ L : E →L[ℝ] ℝ,
+    μ.map L = gaussianReal (μ[L]) (Var[L ; μ]).toNNReal
 
 end Def
 
-instance isGaussian_gaussianReal (m : ℝ) (v : ℝ≥0) : IsGaussian (gaussianReal m v) where
-  map_eq_gaussianReal L := by
-    have : (L : ℝ → ℝ) = fun x ↦ L 1 * x := by
-      ext x
-      have : x = x • 1 := by simp
-      conv_lhs => rw [this, L.map_smul, smul_eq_mul, mul_comm]
-    rw [this]
-    exact ⟨L 1 * m, ⟨(L 1) ^ 2, sq_nonneg _⟩ * v, gaussianReal_map_const_mul _⟩
-
-variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
-  {mE : MeasurableSpace E} [BorelSpace E] [SecondCountableTopology E]
-
-instance {x : E} : IsGaussian (Measure.dirac x) where
-  map_eq_gaussianReal L := by
-    refine ⟨L x, 0, ?_⟩
-    simp only [gaussianReal_zero_var]
-    rw [Measure.map_dirac (by fun_prop)]
-
-instance {μ : Measure E} [IsGaussian μ] : IsProbabilityMeasure μ where
-  measure_univ := by
-    let L : E →L[ℝ] ℝ := Nonempty.some inferInstance
-    have : μ.map L Set.univ = 1 := by simp [μ.map_eq_gaussianReal L]
-    simpa [Measure.map_apply (by fun_prop : Measurable L) .univ] using this
-
-@[simp]
-lemma meanMap_dirac (L : E →L[ℝ] ℝ) (x : E) :
-    (Measure.dirac x).meanMap L = L x := by
-  have h := Measure.map_eq_gaussianReal (Measure.dirac 0) L
-  rw [Measure.map_dirac (by fun_prop), ← gaussianReal_zero_var] at h
-  sorry
-
-@[simp]
-lemma varMap_dirac (L : E →L[ℝ] ℝ) (x : E) :
-    (Measure.dirac x).varMap L = 0 := by
-  have h := Measure.map_eq_gaussianReal (Measure.dirac 0) L
-  rw [Measure.map_dirac (by fun_prop), ← gaussianReal_zero_var] at h
-  sorry
-
-lemma isGaussian_map_prod_add {μ ν : Measure E} [IsGaussian μ] [IsGaussian ν] :
-    IsGaussian ((μ.prod ν).map (fun p ↦ p.1 + p.2)) where
-  map_eq_gaussianReal := by
-    intro L
-    rw [Measure.map_map (by fun_prop) (by fun_prop)]
-    have : (L ∘ fun (p : E × E) ↦ p.1 + p.2)
-        = (fun p : ℝ × ℝ ↦ p.1 + p.2) ∘ (Prod.map L L) := by ext; simp
-    rw [this, ← Measure.map_map (by fun_prop) (by fun_prop)]
-    refine ⟨μ.meanMap L + ν.meanMap L, μ.varMap L + ν.varMap L, ?_⟩
-    rw [← Measure.map_prod_map, μ.map_eq_gaussianReal L, ν.map_eq_gaussianReal L,
-      gaussianReal_map_prod_add]
-    · fun_prop
-    · fun_prop
-
-lemma isGaussian_conv {μ ν : Measure E} [IsGaussian μ] [IsGaussian ν] :
-    IsGaussian (μ ∗ ν) := isGaussian_map_prod_add
-
-
-
+theorem mgf_id_gaussianReal {μ : ℝ} {v : ℝ≥0} :
+    mgf (fun x ↦ x) (gaussianReal μ v) = fun t ↦ rexp (μ * t + v * t ^ 2 / 2) := by
+  ext t
+  suffices (mgf id (gaussianReal μ v) t : ℂ) = rexp (μ * t + ↑v * t ^ 2 / 2) from mod_cast this
+  rw [← complexMGF_ofReal, complexMGF_id_gaussianReal, mul_comm μ]
+  norm_cast
 
 lemma integrable_exp_mul_gaussianReal (t : ℝ) :
     Integrable (fun x ↦ rexp (t * x)) (gaussianReal μ v) := by
@@ -153,6 +111,88 @@ lemma integrable_exp_mul_gaussianReal (t : ℝ) :
 lemma integrableExpSet_id_gaussianReal : integrableExpSet id (gaussianReal μ v) = Set.univ := by
   ext
   simpa [integrableExpSet] using integrable_exp_mul_gaussianReal _ _ _
+
+@[simp]
+lemma integrableExpSet_id_gaussianReal' :
+    integrableExpSet (fun x ↦ x) (gaussianReal μ v) = Set.univ :=
+  integrableExpSet_id_gaussianReal _ _
+
+@[simp]
+lemma integral_id_gaussianReal :
+    ∫ x, x ∂gaussianReal μ v = μ := by
+  rw [← deriv_mgf_zero, mgf_id_gaussianReal]
+  · rw [_root_.deriv_exp (by fun_prop)]
+    simp only [mul_zero, ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow, zero_div,
+      add_zero, Real.exp_zero, one_mul]
+    rw [deriv_add (by fun_prop) (by fun_prop)]
+    simp only [deriv_div_const, differentiableAt_const, differentiableAt_id', DifferentiableAt.pow,
+      deriv_mul, deriv_const', ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow, mul_zero,
+      deriv_pow'', Nat.cast_ofNat, Nat.add_one_sub_one, pow_one, deriv_id'', mul_one, add_zero,
+      zero_div]
+    change deriv (fun x ↦ μ * x) 0 = μ
+    rw [deriv_mul (by fun_prop) (by fun_prop)]
+    simp
+  · simp
+
+@[simp]
+lemma variance_id_gaussianReal : Var[fun x ↦ x ; gaussianReal μ v] = v := by
+  rw [variance_eq_integral measurable_id'.aemeasurable]
+  simp only [integral_id_gaussianReal]
+  calc ∫ ω, (ω - μ) ^ 2 ∂gaussianReal μ v
+  _ = ∫ ω, ω ^ 2 ∂(gaussianReal μ v).map (fun x ↦ x + -μ) := by
+    rw [integral_map]
+    · simp [sub_eq_add_neg]
+    · fun_prop
+    · refine Measurable.aestronglyMeasurable <| by fun_prop
+  _ = ∫ ω, ω ^ 2 ∂(gaussianReal 0 v) := by simp [gaussianReal_map_add_const]
+  _ = iteratedDeriv 2 (mgf (fun x ↦ x) (gaussianReal 0 v)) 0 := by
+    rw [iteratedDeriv_mgf_zero] <;> simp
+  _ = v := by
+    simp_rw [mgf_id_gaussianReal, zero_mul, zero_add]
+    rw [iteratedDeriv_succ, iteratedDeriv_one]
+    have : deriv (fun t ↦ rexp (v * t ^ 2 / 2)) = fun t ↦ v * t * rexp (v * t ^ 2 / 2) := by
+      ext t
+      rw [_root_.deriv_exp (by fun_prop)]
+      simp only [deriv_div_const, differentiableAt_const, differentiableAt_id',
+        DifferentiableAt.pow, deriv_mul, deriv_const', zero_mul, deriv_pow'', Nat.cast_ofNat,
+        Nat.add_one_sub_one, pow_one, deriv_id'', mul_one, zero_add]
+      ring
+    rw [this, deriv_mul (by fun_prop) (by fun_prop)]
+    simp only [ne_eq, OfNat.ofNat_ne_zero, not_false_eq_true, zero_pow, mul_zero, zero_div,
+      Real.exp_zero, mul_one, zero_mul, add_zero]
+    change deriv (fun x : ℝ ↦ v * x) 0 = v
+    rw [deriv_mul (by fun_prop) (by fun_prop)]
+    simp
+
+@[simp]
+lemma variance_id_gaussianReal' : Var[id ; gaussianReal μ v] = v :=
+  variance_id_gaussianReal _ _
+
+instance isGaussian_gaussianReal (m : ℝ) (v : ℝ≥0) : IsGaussian (gaussianReal m v) where
+  map_eq_gaussianReal L := by
+    have : (L : ℝ → ℝ) = fun x ↦ L 1 * x := by
+      ext x
+      have : x = x • 1 := by simp
+      conv_lhs => rw [this, L.map_smul, smul_eq_mul, mul_comm]
+    rw [this, gaussianReal_map_const_mul, integral_mul_left]
+    simp only [integral_id_gaussianReal]
+    rw [variance_mul, Real.toNNReal_mul (by positivity)]
+    congr
+    · simp only [left_eq_sup]
+      positivity
+    · simp
+
+variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E]
+  {mE : MeasurableSpace E} [BorelSpace E] [SecondCountableTopology E]
+
+instance {x : E} : IsGaussian (Measure.dirac x) where
+  map_eq_gaussianReal L := by rw [Measure.map_dirac (by fun_prop)]; simp
+
+instance {μ : Measure E} [IsGaussian μ] : IsProbabilityMeasure μ where
+  measure_univ := by
+    let L : E →L[ℝ] ℝ := Nonempty.some inferInstance
+    have : μ.map L Set.univ = 1 := by simp [IsGaussian.map_eq_gaussianReal L]
+    simpa [Measure.map_apply (by fun_prop : Measurable L) .univ] using this
 
 lemma memLp_id_gaussianReal (p : ℝ≥0) : MemLp id p (gaussianReal μ v) :=
   memLp_of_mem_interior_integrableExpSet (by simp) p
@@ -170,11 +210,38 @@ lemma IsGaussian.memLp_continuousLinearMap (μ : Measure E) [IsGaussian μ] (L :
     (p : ℝ≥0∞) (hp : p ≠ ∞) :
     MemLp L p μ := by
   suffices MemLp (id ∘ L) p μ from this
-  rw [← memLp_map_measure_iff, μ.map_eq_gaussianReal L]
+  rw [← memLp_map_measure_iff, IsGaussian.map_eq_gaussianReal L]
   · convert memLp_id_gaussianReal _ _ p.toNNReal
     simp [hp]
   · exact Measurable.aestronglyMeasurable <| by fun_prop
   · fun_prop
+
+lemma isGaussian_map_prod_add {μ ν : Measure E} [IsGaussian μ] [IsGaussian ν] :
+    IsGaussian ((μ.prod ν).map (fun p ↦ p.1 + p.2)) where
+  map_eq_gaussianReal := by
+    intro L
+    have h1 : ((μ.prod ν).map (fun p ↦ p.1 + p.2)).map L
+        = ((μ.map L).prod (ν.map L)).map (fun p ↦ p.1 + p.2) := by
+      rw [Measure.map_map (by fun_prop) (by fun_prop)]
+      have : (L ∘ fun (p : E × E) ↦ p.1 + p.2)
+          = (fun p : ℝ × ℝ ↦ p.1 + p.2) ∘ (Prod.map L L) := by ext; simp
+      rw [this, ← Measure.map_map (by fun_prop) (by fun_prop),
+        ← Measure.map_prod_map]
+      · fun_prop
+      · fun_prop
+    have : ∫ x, L x ∂((μ.prod ν).map (fun p ↦ p.1 + p.2))
+          = ∫ x, x ∂(((μ.map L).prod (ν.map L)).map (fun p ↦ p.1 + p.2)) := by
+        rw [← h1, integral_map (φ := L)]
+        · fun_prop
+        · exact measurable_id.aestronglyMeasurable
+    rw [h1, this, ← variance_id_map (by fun_prop), h1, IsGaussian.map_eq_gaussianReal L,
+      IsGaussian.map_eq_gaussianReal L, gaussianReal_map_prod_add]
+    congr
+    · simp
+    · simp [variance_nonneg]
+
+lemma isGaussian_conv {μ ν : Measure E} [IsGaussian μ] [IsGaussian ν] :
+    IsGaussian (μ ∗ ν) := isGaussian_map_prod_add
 
 section CharFun
 
@@ -230,8 +297,9 @@ end CharFun
 
 section Centered
 
-def IsCentered (μ : Measure E) [IsGaussian μ] : Prop := ∀ L : E →L[ℝ] ℝ, μ.meanMap L = 0
+def IsCentered (μ : Measure E) [IsGaussian μ] : Prop := ∀ L : E →L[ℝ] ℝ, ∫ x, L x ∂μ = 0
 
+omit [SecondCountableTopology E] in
 lemma isCentered_dirac_zero : IsCentered (Measure.dirac (0 : E)) := by intro L; simp
 
 end Centered
@@ -239,7 +307,7 @@ end Centered
 section IsDegenerate
 
 def IsDegenerate (μ : Measure E) [IsGaussian μ] : Prop :=
-  ∃ L : E →L[ℝ] ℝ, L ≠ 0 ∧ μ.varMap L = 0
+  ∃ L : E →L[ℝ] ℝ, L ≠ 0 ∧ Var[L ; μ] = 0
 
 lemma isDegenerate_dirac (x : E) : IsDegenerate (Measure.dirac x) := by
   obtain ⟨L, hL⟩ : ∃ L : E →L[ℝ] ℝ, L ≠ 0 := by
